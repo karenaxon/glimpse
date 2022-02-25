142487a9670cf64612845e805c13591ab8faf096
Kaxon <karenaxon@users.noreply.github.com>
2022-02-25 11:22:44 -0800

    A a note: troubleshooting = Cannot read properties of null - reading 'jsonType' - in Sanity studio

---
+++ backend/schemas/user.js
@@ -0,0 +1,17 @@
+export default{
+  name: 'user',
+  title: 'User',
+  type: 'documents',
+  fields: [
+    {
+      name: 'userName',
+      title: 'UserName',
+      type: 'string'
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'string'
+    },
+  ],
+};
