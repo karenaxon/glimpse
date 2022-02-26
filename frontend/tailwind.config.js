f0a3064ed81eed9b8efc8f63ff0227be5d86127e
Kaxon <karenaxon@users.noreply.github.com>
2022-02-25 16:44:16 -0800

    Update tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -48,6 +48,7 @@ module.exports = {
         mainColor: '#FBF8F9',
         secondaryColor: '#F0F0F0',
         blackOverlay: 'rgba(0, 0 ,0 ,0.7)',
+        blueColor: '#0079C6'
       },
       keyframes: {
         'slide-in': {
