c1185d14b69d757eb5b0c9e9fdef66bfb448c7b2
Kaxon <karenaxon@users.noreply.github.com>
2022-03-06 13:18:34 -0800

    Add loading spinner component

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import  { Circles } from 'react-loader-spinner';
+
+function Spinner ({ message }) {
+  return (
+    <div className='flex flex-cold justify-center items-center w-full h-full'>
+      <Circles 
+        color="#00BFFF"
+        height={50}
+        width={200}
+        className='m-f'
+      />
+
+      <p className='text-lg text-center px-2'>{message}</p>
+    </div>
+  )
+}
+
+export default Spinner;
