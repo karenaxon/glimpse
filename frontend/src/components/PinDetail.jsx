a551a3c8fbb1217c95543e1cae0e4ec2f47c8d30
Kaxon <karenaxon@users.noreply.github.com>
2022-03-09 09:31:48 -0800

    Add commenting on a pin capabilty

--- frontend/src/components/PinDetail.jsx
+++ frontend/src/components/PinDetail.jsx
@@ -64,6 +64,42 @@ const PinDetail = ({ user }) => {
             >
               <MdDownloadForOffline />
             </a>
+            <div>
+            <h1 className="text-4xl font-bold break-words mt-3">
+              {pinDetail.title}
+            </h1>
+            <p className="mt-3">
+              {pinDetail.about}
+            </p>
+            <h2 className="mt-5 text-2xl">Comments</h2>
+            <div className="max-h-370 overflow-y-auto">
+              {pinDetail?.comments?.map((comment, i) => (
+                <div className="flex gap-2 mt-5 items-center bg-white rounded-lg" key={i}>
+                  <img
+                    src={comment.postedBy.image}
+                    alt='user-profile'
+                    className="w-10 h-10 rounded-full cursor-pointer"
+                  />
+                  <div className="flex flex-col">
+                    <p className="font-bold">{comment.postedBy.userName}</p>
+                    <p>{comment.comment}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div>
+              <Link to={`/user-profile/${user._id}`}>
+                <img src={user.image} className="w-10 h-10 rounded-full cursor-pointer" alt="user-profile" />
+              </Link>
+              <input
+              className="flex-1 border-gray-100 outline-none border-2 p-2 rounded 2xl focus:border-gray-300"
+              type='text'
+              placeholder="Add a comment"
+              value={comment}
+              onChange={(e) => setComment(e.target.value)}
+              />
+            </div>
+          </div>
           </div>
         </div>
       </div>
