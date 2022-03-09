ebab0c2a8bcac0587eb01c5752956879829b22f3
Kaxon <karenaxon@users.noreply.github.com>
2022-03-09 10:50:21 -0800

    Add spinner to loading profile

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { useParams, useNavigate } from 'react-router-dom';
+import { GoogleLogout } from 'react-google-login';
+import { client } from '../client';
+import Spinner from './Spinner';
 
 const UserProfile = () => {
+  const [user, setUser] = useState();
+  
+  const navigate = useNavigate();
+  const { userId } = useParams();
+
+  if(!user) {
+    return <Spinner message="Loading profile..."/>
+  }
+
   return (
-    <div>UserProfile</div>
+    <div>
+    UserProfile
+    </div>
   )
 }
 
